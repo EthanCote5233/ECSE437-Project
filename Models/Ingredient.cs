6cf99a3a5cb4c9032a91a7e82a7a653ffef5ca2d
EthanCote5233 <ECote523@Gmail.com>
2022-11-30 13:16:37 -0500

    Add Ingredients and Recipes
    Change-Id: I9289d04424ac956ab7796beae6c110f391319199

---
+++ Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KannotEven.Models
+{
+    public class Ingredient
+    {
+        String name { get; set;} 
+
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KannotEven.Models
+{
+    public class Recipe
+    {
+        String name { get; set; }
+        List<Ingredient> ingredients { get; set; }
+
+        public Recipe(string name, List<Ingredient> ingredients)
+        {
+            this.name = name;
+            this.ingredients = ingredients;
+        }
+
+    }
+}
